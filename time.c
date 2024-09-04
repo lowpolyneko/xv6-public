7c6ea758e03ce5b515e6f9244610f636d958847b
Neko <lowpolyneko@protonmail.ch>
2024-09-04 09:55:35 -0500

    exit instead of returning (and fix formatting)

--- time.c
+++ time.c
@@ -1,19 +1,7 @@
 #include "user.h"
 
-// static inline uint64 __rdtsc(void) {
-// 	uint32 hi, lo;
-// 	__asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-// 	return ( (uint64)lo)|( ((uint64)hi)<<32 );
-// }
-
 int main(int argc, char *argv[]) {
 	uint64 delta, time;
-
-	if (argc < 2) {
-		/* stdout = 1 */
-		printf(1, "specify command to run.");
-		exit();
-	}
 
 	time = __builtin_ia32_rdtsc();
 
@@ -22,11 +10,16 @@ int main(int argc, char *argv[]) {
 		wait();
 	} else {
 		/* child */
-		exec(argv[1], argv + 2);
+		if (argc < 2)
+			exec(0, 0);
+		else
+			exec(argv[1], argv + 2);
 	}
 
 	delta = __builtin_ia32_rdtsc() - time;
-	printf(1, "%s took %d cycles to run.", argv[1], delta);
 
-	return 0;
+	/* stdout = 1 */
+	printf(1, "%s took %d cycles to run.\n", argv[1], delta);
+
+	exit();
 }
